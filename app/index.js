cc4da4328919db10702d65425c6b1042d8fe49da
Christyan Paim <christyan76@outlook.com>
2022-06-08 12:35:50 -0300

    added mysql on docker-compose

--- app/index.js
+++ app/index.js
@@ -1,9 +1,30 @@
+require("dotenv").config();
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.APP_PORT
+
+const mysql = require('mysql2')
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
+
+let names;
+connection.query(`INSERT INTO people(name) VALUES('Some User')`)
+connection.query(`SELECT * FROM people`, (err, result) => {
+  names = result
+})
+connection.end()
 
 app.get('/', (req,res) => {
-  res.send('Hello World!')
+  res.send(
+    `<h1>Full Cycle Rocks!</h1>
+    <h1>${JSON.stringify(names[0])}</h1>
+    `
+  )
 })
 
 app.listen(port, () => {
---
+++ mysql/create.sql
@@ -0,0 +1,6 @@
+
+CREATE DATABASE nodedb;
+
+USE nodedb;
+
+CREATE TABLE people (name varchar(200) not null);
